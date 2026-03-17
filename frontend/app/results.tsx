98158a177d71b314893437938355c138b102f169
Angelica Hanley <angelicahanley2@gmail.com>
2026-03-17 18:22:40 -0400

    created login page & fixed jsx error

---
+++ frontend/app/results.tsx
@@ -0,0 +1,9 @@
+//recommendations page
+import { Text, View } from "react-native";
+export default function Results() {
+  return (
+    <View>
+      <Text>Recommendations Screen!!!</Text>
+    </View>
+  );
+}
